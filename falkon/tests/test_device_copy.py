a4d416426b25e8c0496bf7d2f4676dedd5e0465e
gmeanti <giacomo.meanti@gmail.com>
2021-08-21 16:00:20 +0200

    Upd. tests

--- falkon/tests/test_device_copy.py
+++ falkon/tests/test_device_copy.py
@@ -24,17 +24,6 @@ def large_mat():
     return torch.from_numpy(gen_random(2*n, d, 'float64', False, seed=92))
 
 
-def test_copy_h2d_dtype(mat):
-    in_mat: torch.Tensor = fix_mat(mat, np.float64, "F", device="cuda", copy=True, numpy=False)
-    output = torch.empty_strided(in_mat.size(), in_mat.stride(), dtype=torch.float32, device="cpu")
-
-    opt = FalkonOptions(max_gpu_mem=0.0)
-    with memory_checker(opt) as new_opt:
-        output.copy_(in_mat)
-
-    torch.testing.assert_allclose(in_mat, output.cpu(), rtol=1e-5, atol=1e-5)
-
-
 @pytest.mark.skipif(not decide_cuda(), reason="No GPU found.")
 @pytest.mark.parametrize("order", ["F", "C"])
 def test_copy_host_to_dev(mat, order):
@@ -71,7 +60,7 @@ def test_copy_dev_to_host(mat, order):
     [(100, 1), (100, 10)],
     [(1, 1), (1, 1)],
     [(500, 100), (600, 100)]])
-def test_diff_stride(mat, order, in_dev, size, out_size):
+def test_diff_sizes(mat, order, in_dev, size, out_size):
     if in_dev == "cuda":
         out_dev = "cpu"
     else:
@@ -80,9 +69,9 @@ def test_diff_stride(mat, order, in_dev, size, out_size):
     in_mat = in_mat[:size[0], :size[1]]
 
     if order == "F":
-        output = torch.empty_strided(out_size, (1, out_size[0]), dtype=in_mat.dtype, device=out_dev)
+        output = torch.empty_strided(out_size, (1, out_size[0]), dtype=in_mat.dtype, device=out_dev) * 100.
     else:
-        output = torch.empty_strided(out_size, (out_size[0], 1), dtype=in_mat.dtype, device=out_dev)
+        output = torch.empty_strided(out_size, (out_size[0], 1), dtype=in_mat.dtype, device=out_dev) * 100.
     output = output[:size[0], :size[1]]
 
     opt = FalkonOptions(max_gpu_mem=0.0)
@@ -91,3 +80,24 @@ def test_diff_stride(mat, order, in_dev, size, out_size):
 
     torch.testing.assert_allclose(in_mat.cpu(), output.cpu(), rtol=1e-15, atol=1e-15)
 
+
+@pytest.mark.skipif(not decide_cuda(), reason="No GPU found.")
+@pytest.mark.parametrize("order", ["F", "C"])
+@pytest.mark.parametrize("in_dev", ["cpu", "cuda"])
+def test_wrong_stride(mat, order, in_dev):
+    if in_dev == "cuda":
+        out_dev = "cpu"
+    else:
+        out_dev = "cuda"
+    in_mat: torch.Tensor = fix_mat(mat, np.float64, order=order, device=in_dev, copy=True, numpy=False)
+
+    if order == "F":
+        output = torch.empty_strided(in_mat.size(), (in_mat.stride(0), 1), dtype=in_mat.dtype, device=out_dev) * 100.
+    else:
+        output = torch.empty_strided(in_mat.size(), (1, in_mat.stride(1)), dtype=in_mat.dtype, device=out_dev) * 100.
+
+    opt = FalkonOptions(max_gpu_mem=0.0)
+    with memory_checker(opt) as new_opt:
+        copy(in_mat, output)
+
+    torch.testing.assert_allclose(in_mat.cpu(), output.cpu(), rtol=1e-15, atol=1e-15)
